e426e955f5ca793265cb002cd54d3ce29680672c
Aawaz Gyawali <awazgyawali@gmail.com>
2019-07-29 15:00:00 +0545

    Maps added

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,14 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  [GMSServices provideAPIKey:@"YOUR KEY HERE"];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
